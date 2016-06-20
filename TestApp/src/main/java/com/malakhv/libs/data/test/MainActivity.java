f568f7dc07c53a4208fc5690847733750985773e
mikhail.malakhov <Mikhail.Malakhov@yotadevices.com>
2016-06-20 23:39:33 +0300

    Added TestApp and xDataLib modules

---
+++ TestApp/src/main/java/com/malakhv/libs/data/test/MainActivity.java
@@ -0,0 +1,149 @@
+package com.malakhv.libs.data.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.malakhv.data.BitwiseSet;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private final static String TAG = "LogTest";
+
+    /** The Log view. */
+    private TextView mLog = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLog = (TextView) this.findViewById(R.id.log);
+        log("App started...");
+    }
+
+    /**
+     * Create main menu for this activity.
+     * */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu); return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item != null ? item.getItemId() : 0;
+        if (id == R.id.act_clear) {
+            clear(); return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Clear the log.
+     * */
+    private void clear() { mLog.setText(""); }
+
+    /**
+     * Put specified strings to log.
+     * */
+    private void log(String... strings) {
+        if (strings == null || strings.length <= 0) return;
+        for (String str: strings) {
+            mLog.append(str + "\n");
+        }
+    }
+
+    /**
+     * Called when a view has been clicked.
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        final int id = v != null ? v.getId() : 0;
+        if (id == R.id.btn_1) { action1(); }
+        if (id == R.id.btn_2) { action2(); }
+        if (id == R.id.btn_3) { action3(); }
+        if (id == R.id.btn_4) { action4(); }
+    }
+
+    /** The action for button 1. */
+    private void action1() {
+        clear(); log("Action 1:");
+
+        // Default constructor
+        BitwiseSet set = new BitwiseSet();
+        log(set.toString());
+
+        // All bits are off
+        set = new BitwiseSet(BitwiseSet.BITWISE_VALUE_ALL_OFF);
+        log(set.toString());
+
+        // All bits are on
+        set = new BitwiseSet(BitwiseSet.BITWISE_VALUE_ALL_ON);
+        log(set.toString());
+
+        // To byte array test
+        byte[] bits = set.toBytes();
+        StringBuilder sBuilder = new StringBuilder();
+        for (byte bit: bits) {
+            sBuilder.append(String.valueOf(bit));
+        }
+        log(sBuilder.toString());
+
+        // Test mask
+        int mask = BitwiseSet.Bitwise.getMask(31);
+        log(Integer.toBinaryString(mask));
+        set.set(mask, false);
+        log(set.toString());
+
+        // Test toString
+        log("Test - toString");
+        log(set.toString());
+
+        int mask_0 = 1;
+        int mask_1 = 2;
+        int mask_2 = 4;
+
+        // Invert test
+        //set.not();
+        //log(set.toString());
+        //set.invert(mask_2);
+        //log(set.toString());
+
+        //set.setBit(2, true);
+        //set.setBit(1, true);
+        //set.setBit(0, true);
+        //set.setBit(1, true);
+        //set.setBit(2, true);
+        //set.setBit(4, true);
+        //set.setBit(30, true);
+        //set.setBit(31, true);
+        //set.setBit(30, false);
+
+        //set.setBit(1, true);
+        //set.setBit(0, true);
+        //set.setBit(4, true);
+        //set.setBit(31, true);
+        //log(set.toString());
+
+
+        //set.set(mask_1, false);
+    }
+
+    /** The action for button 2. */
+    private void action2() {
+        clear(); log("Action 2:");
+    }
+
+    /** The action for button 3. */
+    private void action3() {
+        clear(); log("Action 3:");
+    }
+
+    /** The action for button 4. */
+    private void action4() {
+        clear(); log("Action 4:");
+    }
+}
---
+++ xDataLib/src/com/malakhv/data/BitwiseSet.java
@@ -0,0 +1,312 @@
+/**
+ * Copyright (C) 2013 Mikhail Malakhov <malakhv@live.ru>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * */
+
+package com.malakhv.data;
+
+/**
+ * This class implements abstract bitwise set. Each element is a bit flag and may take the value
+ * {@code true} (turned on) or {@code false} (turned off).
+ *
+ * <p>Individual bits can be examined or set (turned on/off) by using appropriate bit mask. One
+ * {@code BitwiseSet} may be used to modify the contents of another {@code BitwiseSet} through
+ * logical AND, logical inclusive OR, and logical exclusive OR operations.</p>
+ *
+ * <p>For store a bit flags, the {@code BitwiseSet} uses one {@code int} field. By default, all
+ * bits in the set initially have the value {@code false}.</p>
+ *
+ * @author Mikhail.Malakhov [malakhv@live.ru|https://github.com/malakhv]
+ * */
+@SuppressWarnings("unused")
+public class BitwiseSet {
+
+    /** The string tag for debug. */
+    private static final String TAG = BitwiseSet.class.getSimpleName();
+
+    /** The value means all flags are turned off (disabled). This value used by default. */
+    public static final int BITWISE_VALUE_ALL_OFF = 0;
+
+    /** The value means all flags are turned on (enabled). */
+    public static final int BITWISE_VALUE_ALL_ON = -1;
+
+    /** The digits for represent bit value as string. */
+    public static final char[] DIGITS = {'0', '1'};
+
+    /** The inner data of this object (bitwise value). */
+    private int mValue = BITWISE_VALUE_ALL_OFF;
+
+    /**
+     * Constructs a new {@link BitwiseSet} instance with default values. All bits are
+     * initially {@code false}.
+     * */
+    public BitwiseSet() { this(BITWISE_VALUE_ALL_OFF); }
+
+    /**
+     * Constructs a new {@link BitwiseSet} instance. All bits are initially accordingly with
+     * {@code value}.
+     * @param value The initial value for this {@link BitwiseSet}.
+     * */
+    public BitwiseSet(int value) { mValue = value; }
+
+    /** Returns the current value this {@code BitwiseSet}. */
+    public int getValue() { return mValue; }
+
+    /** Sets a new value for this {@code BitwiseSet}. */
+    public void setValue(int value) { mValue = value; }
+
+    /**
+     * Checks the status (true or false, turned on or turned of) of the bits specified by mask.
+     * @param mask The bit mask, a flag or combination of flags.
+     * */
+    public boolean is(int mask) { return Bitwise.is(mValue, mask); }
+
+    /**
+     * Set the new state of the bits (true or false, turned on or turned of) specified by mask.
+     * @param mask The bit mask, a flag or combination of flags.
+     * @return The new value of this {@link BitwiseSet}.
+     * */
+    public int set(int mask, boolean on) { return mValue = Bitwise.set(mValue, mask, on); }
+
+    /**
+     * Inverts the new state of the bits specified by mask.
+     * @param mask The bit mask, a flag or combination of flags.
+     * @return The new value of this {@link BitwiseSet}.
+     * */
+    public int invert(int mask) { return mValue = Bitwise.invert(mValue, mask); }
+
+    /**
+     * Sets all bits {@code false} (turns all bits off).
+     * */
+    public void clear() { setValue(BITWISE_VALUE_ALL_OFF); }
+
+    /**
+     * Bitwise AND operation with another specified {@link BitwiseSet} object.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int and(BitwiseSet bitwiseSet) {
+        if (bitwiseSet == null)
+            throw new IllegalArgumentException(TAG + ": and(): bitwiseSet is null");
+        return and(bitwiseSet.getValue());
+    }
+
+    /**
+     * Bitwise AND operation with another specified {@code value}.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int and(int value) { return mValue = Bitwise.and(mValue, value); }
+
+    /**
+     * Bitwise OR operation with another specified {@link BitwiseSet} object.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int or(BitwiseSet bitwiseSet) {
+        if (bitwiseSet == null)
+            throw new IllegalArgumentException(TAG + ": or(): bitwiseSet is null");
+        return or(bitwiseSet.getValue());
+    }
+
+    /**
+     * Bitwise OR operation with another specified {@code value}.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int or(int value) { return mValue = Bitwise.or(mValue, value); }
+
+    /**
+     * Bitwise XOR operation with another specified {@link BitwiseSet} object.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int xor(BitwiseSet bitwiseSet) {
+        if (bitwiseSet == null)
+            throw new IllegalArgumentException(TAG + ": xor(): bitwiseSet is null");
+        return xor(bitwiseSet.getValue());
+    }
+
+    /**
+     * Bitwise XOR operation with another specified {@code value}.
+     * @return The new value of this {@link BitwiseSet} object.
+     * */
+    public int xor(int value) { return mValue = Bitwise.xor(mValue, value); }
+
+    /**
+     * Bitwise NOT operation, inverts all bits in this {@link BitwiseSet} object.
+     * */
+    public int not() { return mValue = Bitwise.not(mValue); }
+
+    /**
+     * Convert the bits to bytes array.
+     * */
+    public byte[] toBytes() { return Bitwise.toBytes(mValue); }
+
+    /**
+     * Returns a string containing a concise, human-readable description of this object. In this
+     * case, it is value of this {@link BitwiseSet} object in decimal format.
+     * */
+    @Override
+    public String toString() { return String.valueOf(getValue()); }
+
+    /**
+     * Returns a string containing a concise, human-readable description of this object. In this
+     * case, it is binary string representation of the value of this {@link BitwiseSet} object. The
+     * returned string is a concatenation of '0' and '1' characters.
+     * */
+    public String toBinaryString() {
+        int value = getValue();
+        char[] buf = new char[Integer.SIZE];
+        int cursor = buf.length;
+        do {
+            buf[--cursor] = DIGITS[value & 1]; value >>>= 1;
+        } while (cursor > 0);
+        return new String(buf);
+    }
+
+    /**
+     * Returns a string containing a concise, human-readable description of this object. In this
+     * case, it is hexadecimal string representation of the value of this {@link BitwiseSet}
+     * object.
+     * */
+    public String toHexString() {
+        // TODO Need to implement this
+        return null;
+    }
+
+    /**
+     * Compares this instance with the specified object and indicates if they are equal.
+     * @param o The object to compare this instance with.
+     * */
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (this.getClass() != o.getClass()) return false;
+        final BitwiseSet obj = (BitwiseSet) o;
+        return this.getValue() == obj.getValue();
+    }
+
+    /**
+     * Returns an integer hash code for this object. Int this implementation it just equals value
+     * of this {@link BitwiseSet} object.
+     * */
+    @Override
+    public int hashCode() { return getValue(); }
+
+    /**
+     * Class that contains methods for working with bitwise operations.
+     * @author Mikhail Malakhov, 2015
+     * */
+    public static class Bitwise {
+
+        /** This class has only static data. Not needed to create instance. */
+        private Bitwise() {}
+
+        /**
+         * Turns on the bits into {@code value} by specified {@code mask}.
+         * @param value The original value.
+         * @param mask The bit mask.
+         * @return The modified {@code value}.
+         * */
+        public static int turnOn(int value, int mask) { return value | mask; }
+
+        /**
+         * Turns off the bits into {@code value} by specified {@code mask}.
+         * @param value The original value.
+         * @param mask The bit mask.
+         * @return The modified {@code value}.
+         * */
+        public static int turnOff(int value, int mask) {
+            return value & (-1 ^ mask);
+        }
+
+        /**
+         * Checks the bits are turned on by specified {@code mask}.
+         * @param value The original value.
+         * @param mask The bit mask.
+         * @return True, if all bits specified in {@code mask} turned on into {@code value}.
+         * Otherwise {@code false}.
+         * */
+        public static boolean is(int value, int mask) { return (value & mask) == mask; }
+
+        /**
+         * Turns on/off the bits into {@code value} by specified {@code mask}.
+         * @param value The original value.
+         * @param mask The bit mask.
+         * @param on If {@code true}, turns on the specified bits, otherwise turns off.
+         * @return The modified {@code value}.
+         * */
+        public static int set(int value, int mask, boolean on) {
+            if (on) return turnOn(value, mask);
+            else return turnOff(value, mask);
+        }
+
+        /**
+         * Turns on/off the bits into {@code value} by specified {@code mask}.
+         * @param value The original value.
+         * @param mask The bit mask.
+         * @return The modified {@code value}.
+         * */
+        public static int invert(int value, int mask) { return value ^ mask; }
+
+        /**
+         * Bitwise AND operation.
+         * @param value1 The operand one.
+         * @param value2 The operand two.
+         * @return Result of logical AND operation between operand one and operand two.
+         * */
+        public static int and(int value1, int value2) { return value1 & value2; }
+
+        /**
+         * Bitwise OR operation.
+         * @param value1 The operand one.
+         * @param value2 The operand two.
+         * @return Result of logical OR operation between operand one and operand two.
+         * */
+        public static int or(int value1, int value2) { return value1 | value2; }
+
+        /**
+         * Bitwise XOR operation.
+         * @param value1 The operand one.
+         * @param value2 The operand two.
+         * @return Result of logical XOR operation between operand one and operand two.
+         * */
+        public static int xor(int value1, int value2) { return value1 ^ value2; }
+
+        /**
+         * Bitwise NOT operation, inverts all bits in {@code value}.
+         * @param value The original value.
+         * @return Result of logical NOT operation for {@code value}.
+         * */
+        public static int not(int value) { return ~value; }
+
+        /**
+         * Returns the bit mask for bit by specified bit index.
+         * @param index the index of bit
+         * */
+        public static int getMask(int index) {
+            if (index < 0 || index > (Integer.SIZE - 1)) return 0;
+            return  1 << (index);
+        }
+
+        /**
+         * Convert specified integer value to byte array with {@code 0} and {@code 1} bits values.
+         * */
+        public static byte[] toBytes(int value) {
+            final byte[] bytes = new byte[Integer.SIZE];
+            int mask = 1 << bytes.length;
+            for (int i = 0; i < bytes.length; i++, mask >>>= 1 ) {
+                bytes[i] = (byte) (Bitwise.is(value, mask) ? 1 : 0);
+            }
+            return bytes;
+        }
+    }
+}
